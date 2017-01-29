14e738c29cf00abfbb3b2cf51e9e746a36eeb082
Your Name <you@example.com>
2017-01-29 23:33:17 +0300

    add new comment

--- static_src/js/index.js
+++ static_src/js/index.js
@@ -11,20 +11,56 @@ import '../css/index.styl'
 
 // для тестов нужно комментировать
 // import template from '../index.html'
+const moment = require('moment');
+moment.locale('ru');
 
-new Vue({
+let app = new Vue({
   el: '#editor',
   data: {
-    input: '# hello'
+    input: '',
+    seen: true,
+    comments: [
+    	{ name: 'user 1', href: '1', url: '', time: '12.12.2012 12:00', text: marked('# one comment') },
+    	{ name: 'user 2', href: '2', url: '', time: '13.13.2013 13:00', text: marked('# two comment') }, 
+    ]
   },
   computed: {
-    compiledMarkdown: function () {
-      return marked(this.input, { sanitize: true })
+  	now: function () {
+	  return Date.now();
+	},
+    compiledMarkdown: function (text) {
+    	if (text) {
+    		return marked(text, { sanitize: true })
+    	} else {
+      		return marked(this.input, { sanitize: true })
+    	}
     }
   },
   methods: {
     update: _.debounce(function (e) {
-      this.input = e.target.value
-    }, 300)
+      this.input = e.target.value;
+    }, 300),
+    addComment: function (e) {
+    	e.preventDefault();
+    	this.comments.push({
+    		name: 'user 3',
+    		href: '3',
+    		url: '',
+    		time: moment(this.now).format('LLL'),
+    		text: marked(this.input)
+
+    	});
+    	this.input = '';
+  
+    }
+  },
+  watch: {
+
+  },
+  // сеттер:
+  set: function (newText) {
+  	console.log(newText);
   }
 });
+
+console.log(app.seen);
